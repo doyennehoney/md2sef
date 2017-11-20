6350b7c9292e7ef4dc496af60e8d66e335241b8f
doyin.a <doyennehoney@gmail.com>
2017-11-20 11:49:43 +0100

    small changesto the data

--- EPI_DEM/src/Data.java
+++ EPI_DEM/src/Data.java
@@ -2,6 +2,7 @@ import model.Events;
 
 public class Data extends Events{
 	int size;
+	//madechanges to 
 	//int grid[][];
 	double susceptible, infected, recovered;
 	public Data(int size, double susceptible,  double infected, double recovered) {
